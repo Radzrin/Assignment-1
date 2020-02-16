9abedf36d09034be4885d76cdc47af5a5fdca4c2
Radzrin <60394053+Radzrin@users.noreply.github.com>
2020-02-15 23:53:38 -0500

    Add files via upload

--- Question4.java
+++ Question4.java
@@ -3,20 +3,16 @@ import java.util.Scanner;
 public class Question4{
  
  public static void main(String[] args) {
-		
+	 
+		String https;
 		String url;
-		lenght x;
+		
 	
 		Scanner sc = new Scanner(System.in);
 		System.out.println("insert a url ");
 		url  = sc.nextLine();
 		System.out.println(url.trim());
-		
-		if (url = http) {
-		System.out.println("the link is secure");
-		} else {
-		System.out.println("the link is not secure");
-		}
+		 System.out.println(url == https);
 		
 	
 	}
