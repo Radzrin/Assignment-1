2210f167166d772aecb7a2ac455d7a8332dba8c1
Radzrin <60394053+Radzrin@users.noreply.github.com>
2020-02-15 21:41:35 -0500

    Add files via upload

---
+++ Question2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Question2 {
+	
+	public static void main(String[] args) {
+	
+
+		double price;
+		double taxes = 0.15;
+		 double priceFinal;
+		 final double finish;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("What is the price of the item you want to buy?");
+		price  = sc.nextInt();
+
+		priceFinal = price * taxes;
+		finish = priceFinal + price;
+
+		System.out.println("the price is " + finish);
+	}
+	
+}	
